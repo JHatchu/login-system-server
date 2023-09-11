e68874e64e5fe413276080a6d8ea79e00fa5050b
J.Harshada <98948031+JHatchu@users.noreply.github.com>
2023-09-12 01:08:45 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -2,7 +2,6 @@ CREATE DATABASE cms;
 
 CREATE TABLE user_master(
     uid SERIAL PRIMARY KEY,
-    user_id VARCHAR(100) NOT NULL,
     fname VARCHAR(100) NOT NULL,
     lname VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
