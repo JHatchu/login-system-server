2b653e898d86bbbbef6d60ef16660ac83067a5ad
J.Harshada <98948031+JHatchu@users.noreply.github.com>
2023-09-12 01:07:50 +0530

    Update userservice.js
    service file changes

--- service/userservice.js
+++ service/userservice.js
@@ -1,64 +1,60 @@
- const userdao = require('../dao/userdao')
- const jwtGenerator = require('../utils/jwtGenerator')
- const bcrypt = require('bcryptjs')
+const userdao = require('../dao/userdao')
+const jwtGenerator = require('../utils/jwtGenerator')
+const bcrypt = require('bcryptjs')
 
- class userservice {
-     async register(user){
-        const{userId,fname,lname,email,password} = user;
-        console.log("userservice user---->",user);
-        const salt = await bcrypt.genSalt(10);
-        const bcryptpassword = await bcrypt.hash(password,salt);
+class userservice {
+    async register(user){
+       const{fname,lname,email,password} = user;
+       console.log("userservice user---->",user);
+       const salt = await bcrypt.genSalt(10);
+       const bcryptpassword = await bcrypt.hash(password,salt);
 
-        const existedUser = await userdao.getbyemail(email);
+       const existedUser = await userdao.getbyemail(email);
 
-        if(existedUser){
-            return{message:"user exists"}
-        }
+       if(existedUser){
+           return{message:"user exists"}
+       }
 
-        let newUser = await userdao.adduser(userId,fname,lname,email,bcryptpassword);
-        console.log("newUser---->",newUser)
-        const jwtToken = jwtGenerator(newUser);
-        return{jwtToken}
-     }
-
-     async login(user){
-        const {identifier, password} = user;
-        console.log("login====>",user);
-        let getUser;
-try{       
-        if(identifier.includes('@')){
-            getUser = await userdao.getbyemail(identifier);
-            console.log("getUser by email ===>", getUser);
-            
-        }
-        else {
-            getUser = await userdao.getbyuser_id(identifier);
-            console.log("getUser by userId ===>", getUser);
-        }
-
-        if(!getUser){
-            return {message:"user not found"}
-        }
-
-        const validpassword = await bcrypt.compare(password, getUser.password);
-
-        if(!validpassword){
-            return{message:"invalid password!!"};
-        }
-
-        const jwtToken = jwtGenerator(getUser);
-        return {jwtToken};
-     }catch (error) {
-        console.error("Error in login function:", error);
-        return { message: "An error occurred." };
-      }
+       let newUser = await userdao.adduser(fname,lname,email,bcryptpassword);
+       console.log("newUser---->",newUser)
+       const jwtToken = jwtGenerator(newUser);
+       return{jwtToken}
     }
-   
-    
-    
-    
-    
-    
-    
 
- }module.exports = new userservice();
+    async login(user){
+       const {email, password} = user;
+       console.log("login====>",user);
+       let getUser;
+try{       
+      
+           getUser = await userdao.getbyemail(email);
+           console.log("getUser by email ===>", getUser);
+           
+      
+
+       if(!getUser){
+           return {message:"user not found"}
+       }
+
+       const validpassword = await bcrypt.compare(password, getUser.password);
+
+       if(!validpassword){
+           return{message:"invalid password!!"};
+       }
+
+       const jwtToken = jwtGenerator(getUser);
+       return {jwtToken};
+    }catch (error) {
+       console.error("Error in login function:", error);
+       return { message: "An error occurred." };
+     }
+   }
+  
+   
+   
+   
+   
+   
+   
+
+}module.exports = new userservice();
